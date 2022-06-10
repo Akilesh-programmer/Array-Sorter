0c5ca405c889e0ff18286036fe2f6639f4dd97e2
Akilesh-programmer <akileshakileshs1234@gmail.com>
2022-06-10 08:51:39 +0530

    create mechanism to sort the array in descending order.

---
+++ src/com/akilesh/Main.java
@@ -0,0 +1,61 @@
+package com.akilesh;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int[] array = getIntegers(6);
+        printArray(array);
+
+        int[] sortedArray = sortIntegers(array);
+        printArray(sortedArray);
+
+        scanner.close();
+    }
+
+    public static int[] getIntegers(int numberOfIntegers) {
+        System.out.println("Enter " + numberOfIntegers + " integers.\r");
+        int[] array = new int[numberOfIntegers];
+        for (int i = 0; i < array.length; i++) {
+            int readInt = scanner.nextInt();
+            array[i] = readInt;
+        }
+        scanner.close();
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element " + i + " is " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array) {
+        int[] sortedArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            sortedArray[i] = array[i];
+        }
+
+        boolean flag = true;
+        int temp;
+
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < (sortedArray.length - 1); i++) {
+                if (sortedArray[i] < sortedArray[i + 1]) {
+                    temp = sortedArray[i + 1];
+                    sortedArray[i + 1] = sortedArray[i];
+                    sortedArray[i] = temp;
+                    flag = true;
+                }
+            }
+        }
+
+        return sortedArray;
+    }
+}
